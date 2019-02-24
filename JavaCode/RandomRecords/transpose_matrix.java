5cc00360f0aa030fff4b8b814a368fde7d4246a2
MikuSugar <syfangjie@live.cn>
2019-02-24 21:07:07 +0800

    2019/02/24 晚上打卡

---
+++ JavaCode/RandomRecords/transpose_matrix.java
@@ -0,0 +1,36 @@
+package JavaCode.RandomRecords;
+
+public class transpose_matrix
+{
+    public int[][] transpose(int[][] A)
+    {
+        if(A.length==0)return new int[][]{};
+        int[][] result=new int[A[0].length][A.length];
+        for(int i=0;i<A[0].length;i++)
+        {
+            for(int j=0;j<A.length;j++)
+            {
+                result[i][j]=A[j][i];
+            }
+        }
+        return result;
+    }
+}
+/**
+ * https://leetcode-cn.com/contest/weekly-contest-92/problems/transpose-matrix/
+ * 867. 转置矩阵
+ * 给定一个矩阵 A， 返回 A 的转置矩阵。
+ *
+ * 矩阵的转置是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：[[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[1,4,7],[2,5,8],[3,6,9]]
+ * 示例 2：
+ *
+ * 输入：[[1,2,3],[4,5,6]]
+ * 输出：[[1,4],[2,5],[3,6]]
+ */
