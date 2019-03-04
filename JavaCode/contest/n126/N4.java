d361ab004a58fb8e2d852657adfd334289cfc8e7
MikuSugar <syfangjie@live.cn>
2019-03-04 12:27:52 +0800

    2019/03/04 上午补题

--- JavaCode/contest/n126/N4.java
+++ JavaCode/contest/n126/N4.java
@@ -2,13 +2,75 @@ package JavaCode.contest.n126;
 
 /**
  * 比赛期间没有做出
+ * 补题
  */
 public class N4
 {
+
+    public static void main(String[] args)
+    {
+        int[] stones={3,5,1,2,6};
+        System.out.println(new N4().mergeStones(stones,3));
+    }
+
+    final static int MAX=Integer.MAX_VALUE>>1;
     public int mergeStones(int[] stones, int K)
     {
+        int n=stones.length;
+        int[] sum =new int[n+1];//前缀和
+        int[][] dp=new int[n+1][n+1];//dp[i][j]表示第i个石头到第j个石头合并之后的花费
+        int[][][] f=new int[n+1][n+1][n+1];//辅助数组，f[i][j][k]表示第i个石头到第j个石头通过k堆合并，未加上最后一次合并值之前的花费
+        //初始化
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n;j++)
+            {
+                for (int k=1;k<=n;k++)
+                {
+                    f[i][j][k]= MAX;
+                    dp[i][j]= MAX;
+                }
+            }
+        }
+        for (int i=1;i<=n;i++)
+        {
+            f[i][i][1]=dp[i][i]=0;
+            sum[i]=sum[i-1]+stones[i-1];
+        }
 
+        //区间DP
+        for (int len=2;len<=n;len++)//区间长度
+        {
+            for (int l=1;l+len-1<=n;l++)//枚举起点l
+            {
+                int r=l+len-1;//枚举尾巴r
+                for (int k=1;k<len&&k<=K;k++)//枚举分割次数
+                {
+                    for (int i=l;i<r;i++)//枚举分割点
+                    {
+                        if (k==1)
+                        {
+                            f[l][r][k+1]=Math.min(f[l][r][k+1],dp[l][i]+dp[i+1][r]);
+                        }
+                        else
+                        {
+                            f[l][r][k+1]=Math.min(f[l][r][k+1],f[l][i][k]+dp[i+1][r]);
+                        }
+                    }
+                }
+                if(len>=K)
+                {
+                    dp[l][r]=Math.min(dp[l][r],f[l][r][K]+sum[r]-sum[l-1]);
+                }
+                else
+                {
+                    dp[l][r]= MAX;
+                }
+            }
+        }
+        return dp[1][n]== MAX ?-1:dp[1][n];
     }
+
 }
 /**
  *https://leetcode-cn.com/contest/weekly-contest-126/problems/minimum-cost-to-merge-stones/
