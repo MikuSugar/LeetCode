e5446f1bdb7f1e3bbc619e970ee4ab044410570e
MikuSugar <syfangjie@live.cn>
2019-04-09 22:20:18 +0800

    2019/04/09 打卡

---
+++ JavaCode/random_records/N801_900/N804_unique_morse_code_words.java
@@ -0,0 +1,58 @@
+package JavaCode.random_records.N801_900;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class N804_unique_morse_code_words {
+
+    static String[] strs={".-","-...","-.-.","-..",".","..-.","--.","....","..",
+            ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    public int uniqueMorseRepresentations(String[] words) {
+        Set<String> set=new HashSet<>();
+        for (String s:words)
+        {
+            set.add(help(s));
+        }
+        return set.size();
+    }
+
+    private String help(String s) {
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            sb.append(strs[c-'a']);
+        }
+        return sb.toString();
+    }
+}
+/**
+ * https://leetcode-cn.com/problems/unique-morse-code-words/
+ * 国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串， 比如: "a" 对应 ".-", "b" 对应 "-...", "c" 对应 "-.-.", 等等。
+ *
+ * 为了方便，所有26个英文字母对应摩尔斯密码表如下：
+ *
+ * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ * 给定一个单词列表，每个单词可以写成每个字母对应摩尔斯密码的组合。例如，"cab" 可以写成 "-.-..--..."，(即 "-.-." + "-..." + ".-"字符串的结合)。我们将这样一个连接过程称作单词翻译。
+ *
+ * 返回我们可以获得所有词不同单词翻译的数量。
+ *
+ * 例如:
+ * 输入: words = ["gin", "zen", "gig", "msg"]
+ * 输出: 2
+ * 解释:
+ * 各单词翻译如下:
+ * "gin" -> "--...-."
+ * "zen" -> "--...-."
+ * "gig" -> "--...--."
+ * "msg" -> "--...--."
+ *
+ * 共有 2 种不同翻译, "--...-." 和 "--...--.".
+ *
+ *
+ * 注意:
+ *
+ * 单词列表words 的长度不会超过 100。
+ * 每个单词 words[i]的长度范围为 [1, 12]。
+ * 每个单词 words[i]只包含小写字母。
+ */
