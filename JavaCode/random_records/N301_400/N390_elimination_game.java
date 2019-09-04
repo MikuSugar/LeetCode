042515f8e7bf151a20865712fa0a7f4f079ee10d
MikuSugar <syfangjie@live.cn>
2019-09-04 21:13:57 +0800

    2019/09/04 打卡

---
+++ JavaCode/random_records/N301_400/N390_elimination_game.java
@@ -0,0 +1,30 @@
+package JavaCode.random_records.N301_400;
+
+public class N390_elimination_game {
+    public int lastRemaining(int n) {
+        return n==1?1:2*(n/2+1-lastRemaining(n/2));
+    }
+}
+/**
+ * 给定一个从1 到 n 排序的整数列表。
+ * 首先，从左到右，从第一个数字开始，每隔一个数字进行删除，直到列表的末尾。
+ * 第二步，在剩下的数字中，从右到左，从倒数第一个数字开始，每隔一个数字进行删除，直到列表开头。
+ * 我们不断重复这两步，从左到右和从右到左交替进行，直到只剩下一个数字。
+ * 返回长度为 n 的列表中，最后剩下的数字。
+ *
+ * 示例：
+ *
+ * 输入:
+ * n = 9,
+ * 1 2 3 4 5 6 7 8 9
+ * 2 4 6 8
+ * 2 6
+ * 6
+ *
+ * 输出:
+ * 6
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/elimination-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
---
+++ JavaCode/random_records/N501_600/N528_random_pick_with_weight.java
@@ -0,0 +1,64 @@
+package JavaCode.random_records.N501_600;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class N528_random_pick_with_weight {
+    class Solution {
+        private int size;
+        private TreeMap<Integer,Integer> map;
+        private Random random;
+        public Solution(int[] w) {
+            map=new TreeMap<>();
+            for (int i=0;i<w.length;i++)
+            {
+                size+=w[i];
+                map.put(size-1,i);
+            }
+            random=new Random();
+        }
+
+        public int pickIndex() {
+            int r=random.nextInt(size);
+            Map.Entry<Integer, Integer> e = map.ceilingEntry(r);
+            return e.getValue();
+        }
+    }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
+}
+/**
+ * 给定一个正整数数组 w ，其中 w[i] 代表位置 i 的权重，请写一个函数 pickIndex ，它可以随机地获取位置 i，选取位置 i 的概率与 w[i] 成正比。
+ *
+ * 说明:
+ *
+ * 1 <= w.length <= 10000
+ * 1 <= w[i] <= 10^5
+ * pickIndex 将被调用不超过 10000 次
+ * 示例1:
+ *
+ * 输入:
+ * ["Solution","pickIndex"]
+ * [[[1]],[]]
+ * 输出: [null,0]
+ * 示例2:
+ *
+ * 输入:
+ * ["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
+ * [[[1,3]],[],[],[],[],[]]
+ * 输出: [null,0,1,1,1,0]
+ * 输入语法说明：
+ *
+ * 输入是两个列表：调用成员函数名和调用的参数。Solution 的构造函数有一个参数，即数组 w。pickIndex 没有参数。输入参数是一个列表，即使参数为空，也会输入一个 [] 空列表。
+ *
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/random-pick-with-weight
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
