d2e61cfd0e1548f39ab05ffd05d51a361c453dc7
MikuSugar <syfangjie@live.cn>
2019-09-29 18:50:15 +0800

    2019/09/29 打卡

--- JavaCode/contest/fall_2019/N4.java
+++ JavaCode/contest/fall_2019/N4.java
@@ -1,12 +1,75 @@
 package JavaCode.contest.fall_2019;
 
+import java.util.*;
+
 /**
  * author:fangjie
  * time:2019/9/24
  */
 public class N4 {
     public int domino(int n, int m, int[][] broken) {
+        List<Integer>[] graph=getGraph(n,m,broken);
+        int res=0;
+        int[] mat=new int[n*10+m];
+        Arrays.fill(mat,-1);
+        for (int i=0;i<graph.length;i++)
+        {
+            if(graph[i].size()==0)continue;
+            int y=i%10;
+            int x=i/10;
+            if((x+y)%2==0)continue;
+            res+=dfs(i,new boolean[n*10+m],mat,graph);
+        }
+        return res;
+    }
+
+    private int dfs(int cur, boolean[] book, int[] mat, List<Integer>[] graph) {
+        for (int next:graph[cur])
+        {
+            if(book[next])continue;
+            book[next]=true;
+            if(mat[next]==-1||dfs(mat[next],book,mat,graph)==1)
+            {
+                mat[cur]=next;
+                mat[next]=cur;
+                return 1;
+            }
+        }
         return 0;
+    }
+
+
+    private List<Integer>[] getGraph(int n, int m, int[][] broken) {
+        List<Integer>[] res=new List[n*10+m];
+        for (int i=0;i<res.length;i++)res[i]=new ArrayList<>();
+        Set<Integer> br=new HashSet<>();
+        for (int[] b:broken)
+        {
+            br.add(getKey(b[0],b[1]));
+        }
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<m;j++)
+            {
+                int key=getKey(i, j);
+                if(br.contains(key))continue;
+                for (int[] next:nexts)
+                {
+                    int ii=i+next[0];
+                    int jj=j+next[1];
+                    int key1=getKey(ii, jj);
+                    if(ii<0||jj<0||ii>=n||jj>=m||br.contains(key1))continue;
+                    res[key].add(key1);
+                }
+            }
+        }
+        return res;
+    }
+
+    private final static int[][] nexts={{-1,0},{0,-1},{0,1},{1,0}};
+    private int getKey(int x,int y)
+    {
+        return x*10+y;
     }
 }
 /**
