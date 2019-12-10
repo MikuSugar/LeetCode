0950a3b7e0fdbf6fc28f8482612450f43b5562bb
MikuSugar <syfangjie@live.cn>
2019-12-10 11:55:02 +0800

    2019/12/10 打卡

---
+++ JavaCode/contest/n166/N1.java
@@ -0,0 +1,62 @@
+package JavaCode.contest.n166;
+
+/**
+ * author:fangjie
+ * time:2019/12/10
+ */
+public class N1 {
+    public int subtractProductAndSum(int n) {
+        return getDigits(n)-getSum(n);
+    }
+
+    private int getSum(int n) {
+        int res=0;
+        while (n>0)
+        {
+            res+=n%10;
+            n/=10;
+        }
+        return res;
+    }
+
+    private int getDigits(int n) {
+        int res=1;
+        while (n>0)
+        {
+            res*=n%10;
+            n/=10;
+        }
+        return res;
+    }
+}
+/*
+给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+
+ 
+
+示例 1：
+
+输入：n = 234
+输出：15
+解释：
+各位数之积 = 2 * 3 * 4 = 24
+各位数之和 = 2 + 3 + 4 = 9
+结果 = 24 - 9 = 15
+示例 2：
+
+输入：n = 4421
+输出：21
+解释：
+各位数之积 = 4 * 4 * 2 * 1 = 32
+各位数之和 = 4 + 4 + 2 + 1 = 11
+结果 = 32 - 11 = 21
+ 
+
+提示：
+
+1 <= n <= 10^5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
