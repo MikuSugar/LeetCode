eb1dfab9d13bc2e55fbf368b753d547daaa3e023
MikuSugar <syfangjie@live.cn>
2020-02-19 22:56:27 +0800

    2020/02/19 打卡

---
+++ JavaCode/contest/n176/N2_product_of_the_last_k_numbers.java
@@ -0,0 +1,51 @@
+package JavaCode.contest.n176;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * author:fangjie
+ * time:2020/2/17
+ */
+public class N2_product_of_the_last_k_numbers { }
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
+class ProductOfNumbers {
+
+    private List<Integer> list;
+    public ProductOfNumbers() {
+        list=new ArrayList<>(100);
+    }
+
+    public void add(int num) {
+        if(num==0)list.clear();
+        else if(list.isEmpty())list.add(num);
+        else list.add(list.get(list.size()-1)*num);
+    }
+
+    public int getProduct(int k) {
+        if(k>list.size())return 0;
+        if(k==list.size())return list.get(list.size()-1);
+        return list.get(list.size()-1)/list.get(list.size()-1-k);
+    }
+}
+/*
+请你实现一个「数字乘积类」ProductOfNumbers，要求支持下述两种方法：
+
+1. add(int num)
+
+将数字 num 添加到当前数字列表的最后面。
+2. getProduct(int k)
+
+返回当前数字列表中，最后 k 个数字的乘积。
+你可以假设当前列表中始终 至少 包含 k 个数字。
+题目数据保证：任何时候，任一连续数字序列的乘积都在 32-bit 整数范围内，不会溢出。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/product-of-the-last-k-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
---
+++ JavaCode/contest/n176/N3_maximum_number_of_events_that_can_be_attended.java
@@ -0,0 +1,59 @@
+package JavaCode.contest.n176;
+
+/**
+ * author:fangjie
+ * time:2020/2/17
+ */
+public class N3_maximum_number_of_events_that_can_be_attended {
+    public int maxEvents(int[][] events) {
+
+    }
+}
+/*
+给你一个数组 events，其中 events[i] = [startDayi, endDayi] ，表示会议 i 开始于 startDayi ，结束于 endDayi 。
+
+你可以在满足 startDayi <= d <= endDayi 中的任意一天 d 参加会议 i 。注意，一天只能参加一个会议。
+
+请你返回你可以参加的 最大 会议数目。
+
+ 
+
+示例 1：
+
+
+
+输入：events = [[1,2],[2,3],[3,4]]
+输出：3
+解释：你可以参加所有的三个会议。
+安排会议的一种方案如上图。
+第 1 天参加第一个会议。
+第 2 天参加第二个会议。
+第 3 天参加第三个会议。
+示例 2：
+
+输入：events= [[1,2],[2,3],[3,4],[1,2]]
+输出：4
+示例 3：
+
+输入：events = [[1,4],[4,4],[2,2],[3,4],[1,1]]
+输出：4
+示例 4：
+
+输入：events = [[1,100000]]
+输出：1
+示例 5：
+
+输入：events = [[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7]]
+输出：7
+ 
+
+提示：
+
+1 <= events.length <= 10^5
+events[i].length == 2
+1 <= events[i][0] <= events[i][1] <= 10^5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-number-of-events-that-can-be-attended
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
