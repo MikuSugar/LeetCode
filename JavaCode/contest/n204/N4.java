7e676e842573447e8adf1f2a91df27fa71674ebe
MikuSugar <syfangjie@live.cn>
2020-09-19 01:31:31 +0800

    2020/09/19 打卡

--- JavaCode/contest/n204/N4.java
+++ JavaCode/contest/n204/N4.java
@@ -1,18 +1,73 @@
 package JavaCode.contest.n204;
 
+import utils.Parse;
+
 /**
  * author: fangjie
  * email: syfangjie@live.cn
  * date: 2020/8/30 10:23 上午
  */
 public class N4 {
-
-    //TODO:待补题
-    public int numOfWays(int[] nums) {
-        return -1;
+    public static void main(String[] args) {
+        System.out.println(new N4().numOfWays(Parse.parseToIntArray("[3,4,5,1,2]")));
     }
 
+    private final static long[][] dp;
     private final static int MOD=(int) (1e9+7);
+    static {
+        dp=new long[1001][1001];
+        dp[0][0]=1L;
+        for (int i=1; i<dp.length; i++) {
+            dp[i][0]=1L;
+            for (int j=1; j<dp.length; j++) {
+                dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%MOD;
+            }
+        }
+    }
+
+    public int numOfWays(int[] nums) {
+        if (nums.length==1) return 0;
+        Node root=new Node(0);
+        for (int i : nums) insert(root, i);
+        dfs(root);
+        return (root.res-1+MOD)%MOD;
+    }
+
+    private void dfs(Node node) {
+        if (node==null) return;
+        dfs(node.left);
+        dfs(node.right);
+        int lSize=node.left!=null ? node.left.size : 0;
+        int rSize=node.right!=null ? node.right.size : 0;
+        int lRes=node.left!=null ? node.left.res : 1;
+        int rRes=node.right!=null ? node.right.res : 1;
+        node.res=(int) (dp[lSize+rSize][lSize]%MOD*lRes%MOD*rRes%MOD);
+    }
+
+    private void insert(Node root, int value) {
+        if(root==null)return;
+        root.size++;
+        if(value<root.value){
+            if(root.left==null)root.left=new Node(value);
+            else insert(root.left,value);
+        }
+        else {
+            if(root.right==null)root.right=new Node(value);
+            else insert(root.right,value);
+        }
+    }
+
+    static class Node {
+        Node left, right;
+        int value, size, res;
+
+        public Node(int value) {
+            this.value=value;
+            this.size=1;
+            this.res=0;
+        }
+    }
+
 }
 /*
 给你一个数组 nums 表示 1 到 n 的一个排列。我们按照元素在 nums 中的顺序依次插入一个初始为空的二叉查找树（BST）。请你统计将 nums 重新排序后，统计满足如下条件的方案数：重排后得到的二叉查找树与 nums 原本数字顺序得到的二叉查找树相同。
