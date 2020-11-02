29b34d35bf4e1728c5207f2d49e5f6d464db12d6
MikuSugar <syfangjie@live.cn>
2020-11-02 17:41:02 +0800

    update: utils

--- utils/Show.java
+++ utils/Show.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * author: fangjie
@@ -8,35 +9,51 @@ import java.util.Arrays;
  * date: 2020/10/27 6:12 下午
  */
 public  class Show {
-    public static <T> void show(T[] arr)
-    {
-        System.out.println("===================");
-        System.out.println(Arrays.toString(arr));
+
+    public static  void showRow(){
         System.out.println("===================");
     }
-    public static <T> void show(T[][] arr)
-    {
-        System.out.println("===================");
+
+    public static <T> void showT(Collection<T []> collection ) {
+        showRow();
+        collection.forEach(v->System.out.println(Arrays.toString(v)));
+        showRow();
+    }
+
+    public static  void showInt(Collection<int[]> collection ) {
+        showRow();
+        collection.forEach(v->System.out.println(Arrays.toString(v)));
+        showRow();
+    }
+
+    public static <T> void show(T[] arr) {
+        showRow();
+        System.out.println(Arrays.toString(arr));
+        showRow();
+    }
+
+    public static <T> void show(T[][] arr) {
+       showRow();
         for (T[] a:arr)
         {
             System.out.println(Arrays.toString(a));
         }
-        System.out.println("===================");
+       showRow();
     }
-    public static void show(int[] arr)
-    {
-        System.out.println("===================");
+
+    public static void show(int[] arr) {
+        showRow();
         System.out.println(Arrays.toString(arr));
-        System.out.println("===================");
+        showRow();
     }
-    public static void show(int[][] arr)
-    {
-        System.out.println("===================");
+
+    public static void show(int[][] arr) {
+        showRow();
         for (int[] a:arr)
         {
             System.out.println(Arrays.toString(a));
         }
-        System.out.println("===================");
+        showRow();
     }
 
 }
