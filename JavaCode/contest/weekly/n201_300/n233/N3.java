ff16752587732b7aeefbc6adb5bcfc17c2a5088d
MikuSugar <syfangjie@live.cn>
2021-03-22 15:49:52 +0800

    2021/03/22 打卡

--- JavaCode/contest/weekly/n201_300/n233/N3.java
+++ JavaCode/contest/weekly/n201_300/n233/N3.java
@@ -7,13 +7,46 @@ package JavaCode.contest.weekly.n201_300.n233;
  */
 public class N3 {
     public static void main(String[] args) {
-        System.out.println(new N3().maxValue(6,4,395252961));
-    }
-    public int maxValue(int n, int index, int maxSum) {
-       //TODO：
-        return -1;
+        System.out.println(new N3().maxValue(6, 1, 10));
     }
 
+    public int maxValue(int n, int index, int maxSum) {
+        int res=1;
+        int left=res, right=maxSum;
+        while (left<=right) {
+            int mid=(left+right)/2;
+            if (check(mid, n, index, maxSum)) {
+                res=mid;
+                left=mid+1;
+            } else right=mid-1;
+        }
+        return res;
+    }
+
+    private boolean check(int max, int n, int index, int maxSum) {
+        int sum=0;
+        int l=max,lIdx=index;
+        while (l>1&&lIdx>=0)
+        {
+            sum+=l;
+            if(sum>maxSum)return false;
+            l--;
+            lIdx--;
+        }
+        if(lIdx>=0)sum+=lIdx+1;
+        if(sum>maxSum)return false;
+        sum-=max;
+        int r=max,rIdx=index;
+        while (r>1&&rIdx<n){
+            sum+=r;
+            if(sum>maxSum)return false;
+            r--;
+            rIdx++;
+        }
+        if(rIdx!=n)sum+=(n-rIdx);
+        // System.out.println(sum);
+        return sum<=maxSum;
+    }
 }
 /*
 给你三个正整数 n、index 和 maxSum 。你需要构造一个同时满足下述所有条件的数组 nums（下标 从 0 开始 计数）：
