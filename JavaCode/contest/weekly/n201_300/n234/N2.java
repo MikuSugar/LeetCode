e8645271f5dce9fdfb4a449179e069997755951b
MikuSugar <syfangjie@live.cn>
2021-03-28 13:20:29 +0800

    2021/03/28 第234场周赛打卡

---
+++ JavaCode/contest/weekly/n201_300/n234/N2.java
@@ -0,0 +1,77 @@
+package JavaCode.contest.weekly.n201_300.n234;
+
+
+/**
+ * author: fangjie
+ * email: syfangjie@live.cn
+ * date: 2021/3/28 10:17 上午
+ */
+public class N2 {
+    public static void main(String[] args) {
+        System.out.println(new N2().reinitializePermutation(4));
+    }
+    public int reinitializePermutation(int n) {
+        int[] arr=new int[n];
+        int[] tmp=new int[n];
+        for (int i=0;i<n;i++)arr[i]=i;
+        for (int res=1;res<4;res++)
+        {
+            //System.out.println(Arrays.toString(arr));
+            for (int i=0;i<n;i++)
+            {
+                if(i%2==0) tmp[i]=arr[i/2];
+                else if(i%2==1) tmp[i]=arr[n/2+(i-1)/2];
+            }
+            System.arraycopy(tmp, 0, arr, 0, n);
+            boolean flag=true;
+            for (int i=0;i<n;i++)
+            {
+                if(arr[i]!=i){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag)return res;
+        }
+        return -1;
+    }
+}
+/*
+给你一个偶数 n​​​​​​ ，已知存在一个长度为 n 的排列 perm ，其中 perm[i] == i​（下标 从 0 开始 计数）。
+
+一步操作中，你将创建一个新数组 arr ，对于每个 i ：
+
+如果 i % 2 == 0 ，那么 arr[i] = perm[i / 2]
+如果 i % 2 == 1 ，那么 arr[i] = perm[n / 2 + (i - 1) / 2]
+然后将 arr​​ 赋值​​给 perm 。
+
+要想使 perm 回到排列初始值，至少需要执行多少步操作？返回最小的 非零 操作步数。
+
+
+
+示例 1：
+
+输入：n = 2
+输出：1
+解释：最初，perm = [0,1]
+第 1 步操作后，perm = [0,1]
+所以，仅需执行 1 步操作
+示例 2：
+
+输入：n = 4
+输出：2
+解释：最初，perm = [0,1,2,3]
+第 1 步操作后，perm = [0,2,1,3]
+第 2 步操作后，perm = [0,1,2,3]
+所以，仅需执行 2 步操作
+示例 3：
+
+输入：n = 6
+输出：4
+
+
+提示：
+
+2 <= n <= 1000
+n​​​​​​ 是一个偶数
+ */
