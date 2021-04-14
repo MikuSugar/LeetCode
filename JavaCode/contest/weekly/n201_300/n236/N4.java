d2d5563576005bf2e28e6defa3b40541fd0f1a62
mikusugar <syfangjie@live.cn>
2021-04-14 23:37:04 +0800

    2021/04/14 打卡

--- JavaCode/contest/weekly/n201_300/n236/N4.java
+++ JavaCode/contest/weekly/n201_300/n236/N4.java
@@ -1,27 +1,130 @@
 package JavaCode.contest.weekly.n201_300.n236;
 
+import java.util.*;
+
 /**
  * author: fangjie
  * email: syfangjie@live.cn
  * date: 2021/4/11 11:03 上午
  */
 public class N4 {
-
+    //["MKAverage","addElement","addElement","calculateMKAverage","addElement","addElement","calculateMKAverage","addElement","addElement","calculateMKAverage","addElement"]
+    //[[3,1],[58916],[61899],[],[85406],[49757],[],[27520],[12303],[],[63945]]
+    public static void main(String[] args) {
+        MKAverage mkAverage = new MKAverage(3, 1);
+        mkAverage.addElement(58916);
+        mkAverage.addElement(61899);
+        System.out.println(mkAverage.calculateMKAverage());
+        mkAverage.addElement(85406);
+        mkAverage.addElement(49757);
+        System.out.println(mkAverage.calculateMKAverage());
+        mkAverage.addElement(27520);
+        mkAverage.addElement(12303);
+        System.out.println(mkAverage.calculateMKAverage());
+        mkAverage.addElement(63945);
+    }
 }
 class MKAverage {
 
+    private final int m;
+    private final int k;
+
+    private int sum;
+    private int size;
+
+    private Queue<int[]> queue;
+    private int minSum;
+    private TreeSet<int[]> min;
+    private TreeSet<int[]> minT;
+
+    private int maxSum;
+    private TreeSet<int[]> max;
+    private TreeSet<int[]> maxT;
 
 
     public MKAverage(int m, int k) {
-
+        this.m=m;
+        this.k=k;
+        this.sum=this.maxSum=this.minSum=this.size=0;
+        this.queue=new ArrayDeque<>();
+        min=new TreeSet<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]==o2[0])return Integer.compare(o1[1],o2[1]);
+                return Integer.compare(o1[0],o2[0]);
+            }
+        });
+        minT=new TreeSet<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]==o2[0])return Integer.compare(o1[1],o2[1]);
+                return Integer.compare(o1[0],o2[0]);
+            }
+        });
+        max=new TreeSet<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]==o2[0])return Integer.compare(o1[1],o2[1]);
+                return Integer.compare(o2[0],o1[0]);
+            }
+        });
+        maxT=new TreeSet<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]==o2[0])return Integer.compare(o1[1],o2[1]);
+                return Integer.compare(o2[0],o1[0]);
+            }
+        });
     }
 
     public void addElement(int num) {
+        size++;
+        int[] cur = {num, size};
+
+
+        sum+=num;
+        queue.add(cur);
+        if(queue.size()>m){
+            int[] c = queue.poll();
+            sum-=c[0];
+            minT.remove(c);
+            if(min.remove(c))minSum-=c[0];
+            maxT.remove(c);
+            if(max.remove(c))maxSum-=c[0];
+
+        }
+
+        while (min.size()<k&&!minT.isEmpty()){
+            int[] c = minT.pollFirst();
+            minSum+=c[0];
+            min.add(c);
+        }
+        min.add(cur);
+        minSum+=num;
+        while (min.size()>k){
+            int[] c = min.pollLast();
+            minSum-=c[0];
+            minT.add(c);
+        }
+        while (max.size()<k&&!maxT.isEmpty()){
+            int[] c=maxT.pollFirst();
+            maxSum+=c[0];
+            max.add(c);
+        }
+        max.add(cur);
+        maxSum+=num;
+        while (max.size()>k){
+            int[] c=max.pollLast();
+            maxSum-=c[0];
+            maxT.add(c);
+        }
 
     }
 
     public int calculateMKAverage() {
-      //TODO
+        if(queue.size()<m)return -1;
+        System.out.println(Arrays.toString(new int[]{sum,minSum,maxSum}));
+        return (sum-minSum-maxSum)/(m-2*k);
     }
 }
 
