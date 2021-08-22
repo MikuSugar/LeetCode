8d291871595a11ea188ac9e58bc26b7d848e4917
mikusugar <syfangjie@live.cn>
2021-08-22 14:26:44 +0800

    2021/08/22 第255场周赛打卡

--- JavaCode/contest/weekly/n201_300/n255/N2.java
+++ JavaCode/contest/weekly/n201_300/n255/N2.java
@@ -5,7 +5,71 @@ package JavaCode.contest.weekly.n201_300.n255;
  */
 public class N2 {
 
+    public String findDifferentBinaryString(String[] nums) {
+        Node root = new Node();
+        for (String num : nums) {
+            buildTree(root, num.toCharArray());
+        }
+        final String ans = find(root, "", nums[0].length());
+        StringBuilder res = new StringBuilder(ans == null ? "" : ans);
+        for (int i = res.length(); i < nums[0].length(); i++) res.append('0');
+        return res.toString();
+    }
+
+    private String find(Node node, String s, final int n) {
+        if (node == null) {
+            if (s.length() <= n) return s;
+            return null;
+        }
+        final String res = find(node.one, s + '1', n);
+        if (res != null) return res;
+        return find(node.zero, s + '0', n);
+    }
+
+    private void buildTree(Node root, char[] strs) {
+        Node p = root;
+        for (char c : strs) {
+            if (c == '0') {
+                if (p.zero == null) p.zero = new Node();
+                p = p.zero;
+            } else {
+                if (p.one == null) p.one = new Node();
+                p = p.one;
+            }
+        }
+    }
+
+    static class Node {
+        Node zero;
+        Node one;
+    }
 }
 /*
+给你一个字符串数组 nums ，该数组由 n 个 互不相同 的二进制字符串组成，且每个字符串长度都是 n 。请你找出并返回一个长度为 n 且 没有出现 在 nums 中的二进制字符串。如果存在多种答案，只需返回 任意一个 即可。
 
+
+
+示例 1：
+
+输入：nums = ["01","10"]
+输出："11"
+解释："11" 没有出现在 nums 中。"00" 也是正确答案。
+示例 2：
+
+输入：nums = ["00","01"]
+输出："11"
+解释："11" 没有出现在 nums 中。"10" 也是正确答案。
+示例 3：
+
+输入：nums = ["111","011","001"]
+输出："101"
+解释："101" 没有出现在 nums 中。"000"、"010"、"100"、"110" 也是正确答案。
+
+
+提示：
+
+n == nums.length
+1 <= n <= 16
+nums[i].length == n
+nums[i] 为 '0' 或 '1'
  */
--- JavaCode/contest/weekly/n201_300/n255/N3.java
+++ JavaCode/contest/weekly/n201_300/n255/N3.java
@@ -1,11 +1,86 @@
 package JavaCode.contest.weekly.n201_300.n255;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author mikusugar
  */
 public class N3 {
+    private int res;
 
+    public int minimizeTheDifference(int[][] mat, int target) {
+        Set<Integer>[] book = new Set[mat.length];
+        res = Integer.MAX_VALUE;
+        for (int i = 0; i < book.length; i++) book[i] = new HashSet<>();
+        dfs(0, 0, mat, book, target);
+        return res;
+
+    }
+
+    private void dfs(int sum, int level, int[][] mat, Set<Integer>[] book, int target) {
+        if (level == mat.length - 1) {
+            for (int i = 0; i < mat[level].length; i++) {
+                res = Math.min(res, Math.abs(target - sum - mat[level][i]));
+            }
+            return;
+        }
+        for (int i = 0; i < mat[level].length; i++) {
+            int s = sum + mat[level][i];
+            if (book[level].contains(s)) continue;
+            book[level].add(s);
+            dfs(s, level + 1, mat, book, target);
+        }
+    }
 }
 /*
+给你一个大小为 m x n 的整数矩阵 mat 和一个整数 target 。
 
+从矩阵的 每一行 中选择一个整数，你的目标是 最小化 所有选中元素之 和 与目标值 target 的 绝对差 。
+
+返回 最小的绝对差 。
+
+a 和 b 两数字的 绝对差 是 a - b 的绝对值。
+
+
+
+示例 1：
+
+
+
+输入：mat = [[1,2,3],[4,5,6],[7,8,9]], target = 13
+输出：0
+解释：一种可能的最优选择方案是：
+- 第一行选出 1
+- 第二行选出 5
+- 第三行选出 7
+所选元素的和是 13 ，等于目标值，所以绝对差是 0 。
+示例 2：
+
+
+
+输入：mat = [[1],[2],[3]], target = 100
+输出：94
+解释：唯一一种选择方案是：
+- 第一行选出 1
+- 第二行选出 2
+- 第三行选出 3
+所选元素的和是 6 ，绝对差是 94 。
+示例 3：
+
+
+
+输入：mat = [[1,2,9,8,7]], target = 6
+输出：1
+解释：最优的选择方案是选出第一行的 7 。
+绝对差是 1 。
+
+
+提示：
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 70
+1 <= mat[i][j] <= 70
+1 <= target <= 800
  */
