1791150962f9d9401572073452a0960ae9e213dd
mikusugar <syfangjie@live.cn>
2022-02-10 19:24:16 +0800

    2022/02/10 打卡

---
+++ JavaCode/random_records/N1401_1500/N1447_simplified_fractions_scala.scala
@@ -0,0 +1,57 @@
+package JavaCode.random_records.N1401_1500
+
+import scala.annotation.tailrec
+
+/**
+ * @author mikusugar
+ */
+object N1447_simplified_fractions_scala {
+  def simplifiedFractions(n: Int): List[String] = {
+    (1 to n).flatMap(a => {
+      (a + 1 to n).map(b => {
+        if (gcd(a, b) == 1) s"$a/$b"
+        else null
+      })
+    }).filter(_ != null).toList
+  }
+
+  @tailrec
+  def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a
+    else gcd(b, a % b)
+  }
+}
+
+/*
+给你一个整数 n ，请你返回所有 0 到 1 之间（不包括 0 和 1）满足分母小于等于  n 的 最简 分数 。分数可以以 任意 顺序返回。
+
+ 
+
+示例 1：
+
+输入：n = 2
+输出：["1/2"]
+解释："1/2" 是唯一一个分母小于等于 2 的最简分数。
+示例 2：
+
+输入：n = 3
+输出：["1/2","1/3","2/3"]
+示例 3：
+
+输入：n = 4
+输出：["1/2","1/3","1/4","2/3","3/4"]
+解释："2/4" 不是最简分数，因为它可以化简为 "1/2" 。
+示例 4：
+
+输入：n = 1
+输出：[]
+ 
+
+提示：
+
+1 <= n <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/simplified-fractions
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
